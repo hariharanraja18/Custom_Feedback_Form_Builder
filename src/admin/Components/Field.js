0cc3de69db126881704be6fc84b6b4a9b143842e
hariharanraja18 <143488671+hariharanraja18@users.noreply.github.com>
2024-08-26 09:30:29 +0530

    made Field dynamic

--- src/admin/Components/Field.js
+++ src/admin/Components/Field.js
@@ -1,49 +1,34 @@
-import React from 'react'
+import React from 'react';
 import add from '../assets/images/add.png';
 import textarea from '../assets/images/Textarea.png';
 const Field = ({ setShowAdvancedField }) => {
+	const inputType = [
+		'Textarea',
+		'Numeric rating',
+		'Star rating',
+		'Smiley rating',
+		'Single input line',
+		'Radio button',
+		'Categories',
+	];
 	return (
 		<div className="flex flex-col gap-3 w-72 py-2 px-5 h-screen border border-black">
 			<h1 className="font-bold">Add fields</h1>
-			<div className="flex w-full">
-				<img src={textarea} alt="" />
-				<h3 className="flex-grow">Textarea</h3>
-				<button onClick={setShowAdvancedField}>
-					<img className="" src={add} alt="add-btn" />
-				</button>
-			</div>
-			<div className="flex w-full">
-				<img src={textarea} alt="" />
-				<h3 className="flex-grow">Textarea</h3>
-				<img className="" src={add} alt="add-btn" />
-			</div>
-			<div className="flex w-full">
-				<img src={textarea} alt="" />
-				<h3 className="flex-grow">Textarea</h3>
-				<img className="" src={add} alt="add-btn" />
-			</div>
-			<div className="flex w-full">
-				<img src={textarea} alt="" />
-				<h3 className="flex-grow">Textarea</h3>
-				<img className="" src={add} alt="add-btn" />
-			</div>
-			<div className="flex w-full">
-				<img src={textarea} alt="" />
-				<h3 className="flex-grow">Textarea</h3>
-				<img className="" src={add} alt="add-btn" />
-			</div>
-			<div className="flex w-full">
-				<img src={textarea} alt="" />
-				<h3 className="flex-grow">Textarea</h3>
-				<img className="" src={add} alt="add-btn" />
-			</div>
-			<div className="flex w-full">
-				<img src={textarea} alt="" />
-				<h3 className="flex-grow">Textarea</h3>
-				<img className="" src={add} alt="add-btn" />
-			</div>
+			{inputType.map((input) => {
+				return (
+					<div className="flex w-full">
+						<div className='flex flex-1'>
+							<img src={textarea} alt="" />
+							<h3 className="">{input}</h3>
+						</div>
+						<button onClick={setShowAdvancedField}>
+							<img src={add} alt="add-btn" />
+						</button>
+					</div>
+				);
+			})}
 		</div>
 	);
 };
 
-export default Field
+export default Field;
